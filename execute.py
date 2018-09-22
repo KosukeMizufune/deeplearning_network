9533d24482d75764f963644f1dba76c4ff2fc185
kosuke <mfs.coldvdgas4645@gmail.com>
2018-09-22 19:45:42 +0900

    make out_directory arbitary and remove report of std

--- execute.py
+++ execute.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 def run_train(train, valid, model, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1, lr_drop_epoch=20,
-              l2_param=0, max_epoch=40, gpu_id=0):
+              l2_param=0, max_epoch=40, gpu_id=0, result_dir='result'):
     train_iter = iterators.SerialIterator(train, batchsize)
     valid_iter = iterators.SerialIterator(valid, batchsize, repeat=False, shuffle=False)
 
@@ -23,7 +23,7 @@ def run_train(train, valid, model, batchsize=32, start_lr=0.001, lr_drop_ratio=0
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
 
     trainer = training.Trainer(
-        updater, (max_epoch, 'epoch'), out='cifar10_result')
+        updater, (max_epoch, 'epoch'), out=result_dir)
     trainer_extend(trainer, net, lr_drop_ratio, lr_drop_epoch, valid_iter, gpu_id)
     trainer.run()
 
@@ -39,10 +39,7 @@ def trainer_extend(trainer, net, lr_drop_ratio, lr_drop_epoch, valid_iter, gpu_i
     trainer.extend(extensions.snapshot(filename='snapshot_epoch-{.updater.epoch}'))
     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
     trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'conv1/W/data/std',
-         'elapsed_time']))
-    trainer.extend(extensions.ParameterStatistics(net.predictor.base.conv1, {'std': np.std}))
-    trainer.extend(extensions.PlotReport(['conv1/W/data/std'], x_key='epoch', file_name='std.png'))
+        ['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time']))
     trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
     trainer.extend(
         extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
