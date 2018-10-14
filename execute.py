d9aa1847f645fc8287317c027b9aefdcc3eaab65
kosuke <mfs.coldvdgas4645@gmail.com>
2018-10-14 19:37:22 +0900

    save snapshot regularly

--- execute.py
+++ execute.py
@@ -1,12 +1,11 @@
 import chainer
 from chainer import cuda, iterators, optimizers, training
 from chainer.training import extensions, triggers
-import chainer.links as L
 
 
 def run_train(train, valid, net, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1, lr_drop_epoch=20,
               partially_start_lr=0.001, partially_lr_drop_ratio=0.1, finetune_layer=None, freeze_layer=None,
-              l2_param=0, max_epoch=40, gpu_id=0, result_dir='result'):
+              l2_param=0, max_epoch=40, gpu_id=0, result_dir='result', save_trainer_epoch=1, trainer_dir=None):
     # Iterator
     train_iter = iterators.SerialIterator(train, batchsize)
     valid_iter = iterators.SerialIterator(valid, batchsize, repeat=False, shuffle=False)
@@ -27,13 +26,15 @@ def run_train(train, valid, net, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
     trainer = training.Trainer(
         updater, (max_epoch, 'epoch'), out=result_dir)
+    if trainer_dir:
+        chainer.serializers.load_npz(trainer_dir, trainer)
     trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
-                   partially_lr_drop_ratio, finetune_layer, gpu_id)
+                   partially_lr_drop_ratio, finetune_layer, gpu_id, save_trainer_epoch)
     trainer.run()
 
 
 def trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
-                   partially_lr_drop_ratio, finetune_layer, gpu_id):
+                   partially_lr_drop_ratio, finetune_layer, gpu_id, save_trainer_epoch):
     def partially_drop_lr(trainer):
         if finetune_layer is None:
             pass
@@ -49,7 +50,8 @@ def trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
                    trigger=triggers.ManualScheduleTrigger(lr_drop_epoch, 'epoch'))
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.observe_lr(), trigger=(1, 'epoch'))
-    trainer.extend(extensions.snapshot(filename='snapshot_epoch-{.updater.epoch}'))
+    trainer.extend(extensions.snapshot(filename='snapshot_epoch-{.updater.epoch}'),
+                   trigger=(save_trainer_epoch, 'epoch'))
     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
     trainer.extend(extensions.PrintReport(
         ['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'lr', 'elapsed_time']))
