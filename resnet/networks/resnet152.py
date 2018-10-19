2ae3e969cc13d8fd2c95ab710eeec9d11371ee77
kosuke <mfs.coldvdgas4645@gmail.com>
2018-10-19 17:45:59 +0900

    first commit

---
+++ resnet/networks/resnet152.py
@@ -0,0 +1,17 @@
+import chainer
+from chainer import links as L
+
+
+class ResNet152(chainer.Chain):
+    def __init__(self, n_out, init_param=None):
+        super(ResNet152, self).__init__()
+        self.n_out = n_out
+
+        with self.init_scope():
+            self.base = L.ResNet152Layers()
+            self.fc = L.Linear(None, n_out, initialW=init_param)
+
+    def __call__(self, x):
+        h = self.base(x, layers=['pool5'])['pool5']
+        y = self.fc(h)
+        return y
