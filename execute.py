bfed38b2928dd5b5d9b8e5aadf9e31c0adb21924
kosuke <mfs.coldvdgas4645@gmail.com>
2019-02-04 18:45:08 +0900

    change variable name

--- execute.py
+++ execute.py
@@ -4,8 +4,8 @@ from chainer.training import extensions, triggers
 
 
 def run_train(train, valid, net, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1, lr_drop_epoch=20,
-              partially_start_lr=0.001, partially_lr_drop_ratio=0.1, finetune_layer=None, freeze_layer=None,
-              l2_param=0, max_epoch=40, gpu_id=0, result_dir='result', save_trainer_epoch=1, trainer_dir=None):
+              slow_start_lr=0.001, slow_lr_drop_ratio=0.1, slow_layer=None, freeze_layer=None,
+              l2_param=0.0001, max_epoch=30, gpu_id=0, result_dir='result', save_trainer_epoch_interval=5, trainer_dir=None):
     # Iterator
     train_iter = iterators.SerialIterator(train, batchsize)
     valid_iter = iterators.SerialIterator(valid, batchsize, repeat=False, shuffle=False)
@@ -18,9 +18,9 @@ def run_train(train, valid, net, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1
     if l2_param > 0:
         optimizer.add_hook(chainer.optimizer_hooks.WeightDecay(l2_param))
     freeze_setup(net, optimizer, freeze_layer)
-    if finetune_layer:
-        for layer in finetune_layer:
-            layer.update_rule.hyperparam.lr = partially_start_lr
+    if slow_layer:
+        for layer in slow_layer:
+            layer.update_rule.hyperparam.lr = slow_start_lr
 
     # Trainer
     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
@@ -29,21 +29,21 @@ def run_train(train, valid, net, batchsize=32, start_lr=0.001, lr_drop_ratio=0.1
     if trainer_dir:
         chainer.serializers.load_npz(trainer_dir, trainer)
     trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
-                   partially_lr_drop_ratio, finetune_layer, gpu_id, save_trainer_epoch)
+                   slow_lr_drop_ratio, slow_layer, gpu_id, save_trainer_epoch_interval)
     trainer.run()
 
 
 def trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
-                   partially_lr_drop_ratio, finetune_layer, gpu_id, save_trainer_epoch):
-    def partially_drop_lr(trainer):
-        if finetune_layer is None:
+                   slow_lr_drop_ratio, slow_layer, gpu_id, save_trainer_epoch_interval):
+    def slow_drop_lr(trainer):
+        if slow_layer is None:
             pass
         else:
-            for layer in finetune_layer:
-                layer.update_rule.hyperparam.lr *= partially_lr_drop_ratio
+            for layer in slow_layer:
+                layer.update_rule.hyperparam.lr *= slow_lr_drop_ratio
 
     trainer.extend(
-        partially_drop_lr,
+        slow_drop_lr,
         trigger=triggers.ManualScheduleTrigger(lr_drop_epoch, 'epoch')
     )
     trainer.extend(extensions.ExponentialShift('lr', lr_drop_ratio),
@@ -51,7 +51,7 @@ def trainer_extend(trainer, valid_iter, net, lr_drop_ratio, lr_drop_epoch,
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.observe_lr(), trigger=(1, 'epoch'))
     trainer.extend(extensions.snapshot(filename='snapshot_epoch-{.updater.epoch}'),
-                   trigger=(save_trainer_epoch, 'epoch'))
+                   trigger=(save_trainer_epoch_interval, 'epoch'))
     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
     trainer.extend(extensions.PrintReport(
         ['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'lr', 'elapsed_time']))
